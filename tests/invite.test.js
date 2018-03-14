d4d3aad7572a8a39998fcb6f4f78b9db6fe11277
r <camlet22@gmail.com>
2018-03-14 13:11:09 +0100

    test(socket): update socket tests
    https://github.com/eyedea-io/boardapp-website/issues/54

---
+++ tests/invite.test.js
@@ -0,0 +1,69 @@
+import sinon from 'sinon'
+import {assert} from 'chai'
+import {describe, it} from 'mocha'
+import {run} from '@syncano/test'
+import {createSyncanoCoreStub} from './utils'
+
+describe('invite', () => {
+  const meta = {
+    token: process.env.E2E_CLI_ACCOUNT_KEY,
+    user: {
+      id: 5,
+      username: 'fakeName'
+    }
+  }
+
+  it('can create invitation', async () => {
+    const args = {
+      email: 'test@email.com',
+      resource_id: '1234',
+      resource_type: 'company',
+      details: {company_id: 1234}
+    }
+    const fakeInvitation = Object.assign({}, args, {key: 'aasdf123asdf123asdf'})
+    const SyncanoCoreStub = createSyncanoCoreStub({
+      data: {
+        invitations: {
+          fields: () => ({
+            create: sinon.stub().resolves(fakeInvitation)
+          })
+        }
+      }
+    })
+    const mocks = {'@syncano/core': SyncanoCoreStub}
+
+    const result = await run('invite', {args, meta}, {mocks})
+    assert.propertyVal(result, 'code', 200)
+    assert.deepPropertyVal(result, 'data', fakeInvitation)
+  })
+
+  it('can\'t create invitation without being authenticated', async () => {
+    const metaWithoutUser = Object.assign({}, meta, {user: undefined})
+    const result = await run('invite', {args: {}, meta: metaWithoutUser})
+    assert.propertyVal(result, 'code', 401)
+    assert.propertyVal(result.data, 'message', 'Unauthorized.')
+  })
+
+  it('internal error', async () => {
+    const args = {
+      email: 'test@email.com',
+      resource_id: '1234',
+      resource_type: 'company',
+      details: {company_id: 1234}
+    }
+    const SyncanoCoreStub = createSyncanoCoreStub({
+      data: {
+        invitations: {
+          fields: () => ({
+            create: sinon.stub().rejects({response: 'error'})
+          })
+        }
+      }
+    })
+    const mocks = {'@syncano/core': SyncanoCoreStub}
+
+    const result = await run('invite', {args, meta}, {mocks})
+    assert.propertyVal(result, 'code', 400)
+    assert.deepPropertyVal(result.data, 'message', 'error')
+  })
+})
